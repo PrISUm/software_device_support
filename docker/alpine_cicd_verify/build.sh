f61c808a0c7affff7827b747c9ee2fda08559193
Gregory Ling <0b0100011101101100@gmail.com>
2023-09-09 10:37:36 -0500

    Updated notes

--- docker/alpine_cicd_verify/build.sh
+++ docker/alpine_cicd_verify/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Please log in with teamprisum Docker Hub password to push the built docker image when completed."
-docker login -u teamprisum
+echo "It is suggested to configure docker to use a credential helper"
+echo "https://docs.docker.com/engine/reference/commandline/login/#configure-the-credential-store"
+
+docker login -u teamprisum || exit 1
 
 BUILDER=$(docker buildx create --use)
 docker buildx build --platform=linux/amd64,linux/arm64 --push -t teamprisum/alpine_cicd_verify .
